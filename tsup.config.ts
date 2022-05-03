5c5c30f939c913be87d33c641a3bd189bd0d8cc3
EGOIST <hi@egoist.sh>
2022-05-03 21:47:13 +0800

    fix: should bundle `server-node`

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   target: "node14",
-  entry: ["./src/index.ts", "./src/server.ts", "./src/server-prepare.ts"],
+  entry: [
+    "./src/index.ts",
+    "./src/server.ts",
+    "./src/server-prepare.ts",
+    "./src/server-node.ts",
+  ],
   format: ["esm", "cjs"],
   dts: true,
+  splitting: true,
 })
