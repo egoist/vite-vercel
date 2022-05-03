873b776e86093a99d64c0b2981ca9c382de4d8ad
EGOIST <hi@egoist.sh>
2022-05-03 17:19:20 +0800

    feat: Adding `MiddlewareResponse.redirect`

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { validateURL } from "./server-utils"
 
+const REDIRECTS = new Set([301, 302, 303, 307, 308])
+
 export class MiddlewareResponse extends Response {
   static rewrite(destination: string | URL) {
     return new MiddlewareResponse(null, {
@@ -14,6 +16,20 @@ export class MiddlewareResponse extends Response {
       headers: {
         "x-middleware-next": "1",
       },
+    })
+  }
+
+  static redirect(url: string | URL, status = 307) {
+    if (!REDIRECTS.has(status)) {
+      throw new RangeError(
+        'Failed to execute "redirect" on "response": Invalid status code',
+      )
+    }
+
+    const destination = validateURL(url)
+    return new MiddlewareResponse(destination, {
+      headers: { Location: destination },
+      status,
     })
   }
 }
